cd8f56351dc0db024c73cad07c21784899a8241e
JafarMustafazada <jafar.mustafazada@gmail.com>
2024-11-15 22:13:07 +0400

    Added working pixelation logic with threads (but it kinda chaotic)

--- assignment_3/pixelization_x64/src/main.cpp
+++ assignment_3/pixelization_x64/src/main.cpp
@@ -1,76 +1,89 @@
-
 #include <opencv2/opencv.hpp>
-#include <iostream>
 #include <thread>
-#include <vector>
-#include <mutex>
+#include <chrono>
 
+static bool flag1 = true;
+static bool flag2 = true;
 
-void printImage(cv::Mat img) {
-    static std::vector<cv::Mat> images;
+void processBlock(cv::Mat& image, int blockSize, int startRow, int endRow) {
+	cv::Rect wholeBlock(0, 0, image.cols, endRow);
 
-    cv::imshow("Pixelated Image", img);
-    cv::waitKey(30);
+	for (int y = startRow; y < endRow; y += blockSize) {
+		for (int x = 0; x < image.cols; x += blockSize) {
+			cv::Rect blockRect = cv::Rect(x, y, blockSize, blockSize) & wholeBlock;
+			cv::Mat block = image(blockRect);
+			cv::Scalar avgColor = cv::mean(block);
+			block.setTo(avgColor);
+
+			while (flag2) std::this_thread::sleep_for(std::chrono::milliseconds(1));
+			flag2 = true;
+		}
+	}
 }
 
-void pixelateBlock(cv::Mat& img, cv::Rect rect) {
-    cv::Mat block = img(rect);
-    cv::Scalar color = mean(block);
-    block.setTo(color);
+void pixelateWithAnimation(cv::Mat& image, int blockSize, int threadCount) {
+	int blocksInRow = (image.rows + blockSize - 1) / blockSize;
+	int blocksPerThread = (blocksInRow + threadCount - 1) / threadCount;
+	int rowsPerThread = blocksPerThread * blockSize;
+
+	std::thread** threads = new std::thread * [threadCount];
+
+	for (int i = 0; i < threadCount; i++) {
+		int startRow = i * rowsPerThread;
+		int endRow = std::min(startRow + rowsPerThread, image.rows);
+		threads[i] = new std::thread(processBlock, std::ref(image), blockSize, startRow, endRow);
+	}
+
+	for (int i = 0; i < threadCount; i++) {
+		std::thread& t = *threads[i];
+		t.join();
+	}
+
+	flag1 = false;
+	delete[] threads;
 }
 
-void pixelateImage(cv::Mat& img, int squareSize, int numThreads = 1) {
-    std::vector<std::thread> threads;
-    int blockHeight = img.rows / numThreads;
-
-    for (int i = 0; i < numThreads; i++) {
-        int startY = i * blockHeight;
-        int endY = (i + 1) * blockHeight;
-
-        threads.emplace_back([=, &img]() {
-            for (int y = startY; y < endY; y += squareSize) {
-                for (int x = 0; x < img.cols; x += squareSize) {
-                    cv::Rect rect = cv::Rect(x, y, squareSize, squareSize) & cv::Rect(0, 0, img.cols, endY);
-                    pixelateBlock(img, rect);
-                    printImage(img);
-                }
-            }
-            });
-    }
-
-    for (auto& t : threads) {
-        t.join();
-    }
-}
 
 int main(int argc, char** argv) {
-    if (argc != 4) {
-        std::cerr << "Usage: " << argv[0] << " <image_path> <square_size> <mode>" << std::endl;
-        return -1;
-    }
+	if (argc < 4) {
+		std::cerr << "Usage: " << argv[0] << " <image_path> <square_size> <mode> <delay_inms>" << std::endl;
+		return -1;
+	}
 
-    std::string temps = argv[1];
-    int squareSize = std::stoi(argv[2]);
+	std::string temps = argv[1];
+	cv::Mat image = cv::imread(temps);
+	int delayms = 30;
+	int blockSize = std::stoi(argv[2]);
+	int numThreads = std::thread::hardware_concurrency();
+	temps = argv[3];
 
-    cv::Mat image = cv::imread(temps);
-    if (image.empty()) {
-        std::cerr << "Could not open or find the image" << std::endl;
-        return -1;
-    }
+	if (argc > 4) delayms = std::stoi(argv[4]);
+	if (image.empty()) {
+		std::cerr << "Could not open or find the image!" << std::endl;
+		return -1;
+	}
+	if (!numThreads || temps == "S") numThreads = 1;
 
-    temps = argv[3];
-    int numThreads = std::thread::hardware_concurrency();
-    if (!numThreads || temps == "S") numThreads = 1; // Fallback to single thread if hardware_concurrency returns 0 or it is single mode
-    std::cout << "Number of threads: " << numThreads << std::endl;
+	std::cout << "\nNumber of threads: " << numThreads << std::endl;
 
-    cv::namedWindow("Pixelated Image", cv::WINDOW_NORMAL);
-    cv::resizeWindow("Pixelated Image", 800, 600);
+	//cv::namedWindow("Pixelation", cv::WINDOW_NORMAL);
+	//cv::resizeWindow("Pixelation", 800, 600);
+	cv::namedWindow("Pixelation", cv::WINDOW_FREERATIO);
 
-    cv::Mat pixelatedImage = image.clone();
-    pixelateImage(pixelatedImage, squareSize, numThreads);
+	std::thread worker1 = std::thread(pixelateWithAnimation, std::ref(image), blockSize, numThreads);
 
-    cv::imshow("Pixelated Image", pixelatedImage);
-    cv::waitKey(0);
+	while (flag1) {
+		if (flag2) {
+			flag2 = false;
+			std::this_thread::sleep_for(std::chrono::milliseconds(delayms));
+		}
+		cv::imshow("Pixelation", image);
+		cv::waitKey(1);
+	}
 
-    return 0;
+	cv::imwrite("result.jpg", image);
+	cv::imshow("Pixelation", image);
+	cv::waitKey(0);
+
+	return 0;
 }
